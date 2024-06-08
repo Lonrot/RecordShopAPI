161083675c3a15c7e863011fbb5e168c3c248554
Lonrot <angelschmal.1a@gmail.com>
2024-06-08 19:01:23 +0100

    Initial commit. Project structure created, dependencies added.

---
+++ src/test/java/schmalbach/recordshopapi/RecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package schmalbach.recordshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

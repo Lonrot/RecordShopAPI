e2f06fc651a1f71e9aa6cf5be5ebd0ab67894639
Lonrot <angelschmal.1a@gmail.com>
2024-06-10 16:15:05 +0100

    Feature: AlbumRepository added custom query to find Album by name

--- src/main/java/schmalbach/recordshopapi/repository/AlbumRepository.java
+++ src/main/java/schmalbach/recordshopapi/repository/AlbumRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import schmalbach.recordshopapi.model.Album;
 
+import java.util.Optional;
+
 @Repository
 public interface AlbumRepository extends CrudRepository<Album, Long> {
 
+    //Custom query to find album by name , case-insensitive.
+    Optional<Album> findByNameIgnoreCase(String name);
 }

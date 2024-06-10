54705c6fd7bcc399d893638c29160b992d126f91
Lonrot <angelschmal.1a@gmail.com>
2024-06-10 10:12:40 +0100

    Fea: Genre enum created.

---
+++ src/main/java/schmalbach/recordshopapi/model/Genre.java
@@ -0,0 +1,16 @@
+package schmalbach.recordshopapi.model;
+
+public enum Genre {
+    Heavy_metal,
+    Rock,
+    Punk,
+    Phonk,
+    Pop,
+    Electronic,
+    Lo_Fi,
+    Reggaeton,
+    Hip_Hop,
+    Jazz,
+    Blues
+
+}

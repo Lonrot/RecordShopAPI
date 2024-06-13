bfb6c3b02e7ef03bef0829396410dc9f8d8a91ad
Lonrot <angelschmal.1a@gmail.com>
2024-06-13 08:12:50 +0100

    Fix: Change Genre class for better readability.

--- src/main/java/schmalbach/recordshopapi/model/Genre.java
+++ src/main/java/schmalbach/recordshopapi/model/Genre.java
@@ -1,16 +1,5 @@
 package schmalbach.recordshopapi.model;
 
 public enum Genre {
-    Heavy_metal,
-    Rock,
-    Punk,
-    Phonk,
-    Pop,
-    Electronic,
-    Lo_Fi,
-    Reggaeton,
-    Hip_Hop,
-    Jazz,
-    Blues
-
+    Heavy_metal, Rock, Punk, Phonk, Pop, Electronic, Lo_Fi, Reggaeton, Hip_Hop, Jazz, Blues
 }

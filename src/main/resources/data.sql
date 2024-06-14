2ab6621d20bdc05a082fdade05c3a1dbcaeca75c
Lonrot <angelschmal.1a@gmail.com>
2024-06-14 11:38:35 +0100

    Fea: Added SQL Script injection so everytime the App runs the DB is populated

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO ALBUM (id, name, artist, RELEASE_YEAR, label, price, STOCK_QUANTITY, GENRE)
+VALUES (1, 'artist name 1', 'album name 1', 1990, 'label 1', 100.0, 50, 'ROCK');
+
+INSERT INTO ALBUM (id, name, artist, RELEASE_YEAR, label, price, STOCK_QUANTITY, GENRE)
+VALUES (2, 'artist name 2', 'album name 2', 1995, 'label 2', 120.0, 60, 'POP');
+
+INSERT INTO ALBUM (id, name, artist, RELEASE_YEAR, label, price, STOCK_QUANTITY, GENRE)
+VALUES (3, 'artist name 3', 'album name 3', 1985, 'label 3', 80.0, 40, 'JAZZ');
+
+INSERT INTO ALBUM (id, name, artist, RELEASE_YEAR, label, price, STOCK_QUANTITY, GENRE)
+VALUES (4, 'artist name 4', 'album name 4', 2000, 'label 4', 150.0, 30, 'REGGAETON');
+
+INSERT INTO ALBUM (id, name, artist, RELEASE_YEAR, label, price, STOCK_QUANTITY, GENRE)
+VALUES (5, 'artist name 5', 'album name 5', 1970, 'label 5', 90.0, 20, 'BLUES');
